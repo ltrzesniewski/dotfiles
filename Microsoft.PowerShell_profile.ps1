41dc131a8161db92e4344cd652f4e2c51b4cbc65
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-08-30 01:12:12 +0200

    Improve prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,10 @@
-oh-my-posh init pwsh --config "$HOME/dotfiles/prompt.omp.json" | Invoke-Expression
-Import-Module -Name Terminal-Icons
+
+if ($env:WT_SESSION)
+{
+    if (Get-Command oh-my-posh -ErrorAction Ignore)
+    {
+        oh-my-posh init pwsh --config "$PSScriptRoot/prompt.omp.json" | Invoke-Expression
+    }
+
+    Import-Module -Name Terminal-Icons -ErrorAction Ignore
+}
