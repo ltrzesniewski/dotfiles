fe7897384f1584f9ba2f589b38e920d8851e1b81
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2023-05-28 16:34:58 +0200

    Add tab completion for dotnet

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,24 @@
 
-if ($env:WT_SESSION)
-{
-    if (Get-Command oh-my-posh -ErrorAction Ignore)
-    {
+if ($env:WT_SESSION) {
+    if (Get-Command oh-my-posh -ErrorAction Ignore) {
         oh-my-posh init pwsh --config "$PSScriptRoot/prompt.omp.json" | Invoke-Expression
     }
 
     Import-Module -Name Terminal-Icons -ErrorAction Ignore
 }
 
-if (Get-Command kubectl -ErrorAction Ignore)
-{
+if (Get-Command kubectl -ErrorAction Ignore) {
     kubectl completion powershell | Out-String | Invoke-Expression
 }
 
 . "$PSScriptRoot/PowerShell/_rg.ps1"
 $env:RIPGREP_CONFIG_PATH = "$PSScriptRoot/.ripgreprc"
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+ 
