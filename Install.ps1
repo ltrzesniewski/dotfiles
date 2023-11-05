da1ad934ac60744e595b943af775bc2c510eb06d
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2023-11-05 01:25:50 +0100

    Remove vscode-custom
    It's no longer necessary since VSCode 1.84

--- Install.ps1
+++ Install.ps1
@@ -30,10 +30,5 @@ if (Get-Command "cargo" -ErrorAction SilentlyContinue) {
     cargo install bat bottom fd-find
 }
 
-# Get rid of the VSCode confirmation dialog
-New-Item -Path "HKCU:\Software\Classes\vscode-custom\shell\open\command" -Force | Out-Null
-Set-ItemProperty -Path "HKCU:\Software\Classes\vscode-custom" -Name "URL Protocol" -Value ""
-Set-ItemProperty -Path "HKCU:\Software\Classes\vscode-custom\shell\open\command" -Name "(Default)" -Value "wt --window new cmd /c start /min `"`" powershell -ExecutionPolicy Unrestricted -WindowStyle hidden `"$PSScriptRoot\PowerShell\vscode-custom.ps1`" `"%1`""
-
 Write-Output ""
 Write-Output "Done"
