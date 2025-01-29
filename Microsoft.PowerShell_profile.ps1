ae038c391f00d57322ac4c6881d62fbc8039bbc7
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2025-01-30 00:53:44 +0100

    Stop detecting Windows Terminal

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,11 @@
 
-if ($env:WT_SESSION) {
-    if (Get-Command oh-my-posh -ErrorAction Ignore) {
-        oh-my-posh init pwsh --config "$PSScriptRoot/prompt.omp.json" | Invoke-Expression
-    }
+if (Get-Command oh-my-posh -ErrorAction Ignore) {
+    oh-my-posh init pwsh --config "$PSScriptRoot/prompt.omp.json" | Invoke-Expression
+}
 
-    if ((Get-Command atuin -ErrorAction Ignore) -and (Get-Module PSReadLine -ErrorAction Ignore)) {
-        $env:ATUIN_POWERSHELL_PROMPT_OFFSET = -1
-        atuin init powershell --disable-up-arrow | Out-String | Invoke-Expression
-    }
-
-    Import-Module -Name Terminal-Icons -ErrorAction Ignore
+if ((Get-Command atuin -ErrorAction Ignore) -and (Get-Module PSReadLine -ErrorAction Ignore)) {
+    $env:ATUIN_POWERSHELL_PROMPT_OFFSET = -1
+    atuin init powershell --disable-up-arrow | Out-String | Invoke-Expression
 }
 
 if (Get-Command kubectl -ErrorAction Ignore) {
@@ -20,6 +16,8 @@ if (Get-Command rg -ErrorAction Ignore) {
     $env:RIPGREP_CONFIG_PATH = "$PSScriptRoot/.ripgreprc"
     rg --generate complete-powershell | Out-String | Invoke-Expression
 }
+
+Import-Module -Name Terminal-Icons -ErrorAction Ignore
 
 # PowerShell parameter completion shim for the dotnet CLI
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
