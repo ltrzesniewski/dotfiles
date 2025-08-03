b2154af0ed480bdf2a2bcf32ec3e3ab47c09b3f1
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2025-08-03 18:26:49 +0200

    Use the powershell-pr branch for Atuin

--- Install.ps1
+++ Install.ps1
@@ -44,7 +44,7 @@ if (Get-Command "rustup" -ErrorAction SilentlyContinue) {
 }
 
 if (Get-Command "cargo" -ErrorAction SilentlyContinue) {
-    Install-App "atuin" { cargo install --git https://github.com/ltrzesniewski/atuin.git --branch powershell }
+    Install-App "atuin" { cargo install --git https://github.com/ltrzesniewski/atuin.git --branch powershell-pr }
     Install-App "bat" { cargo install bat }
     Install-App "btm" { cargo install bottom }
     Install-App "fd" { cargo install fd-find }
