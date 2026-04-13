4bb4077df2cae1429d07d192b854bc2f22c86e38
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2026-04-13 22:23:38 +0200

    Add cdd

--- PowerShell/Profile.PSv7.ps1
+++ PowerShell/Profile.PSv7.ps1
@@ -30,15 +30,27 @@ function fdf {
 
 # cd find (in current git repo, if any)
 function cdf {
-    $currentDir = (Get-Location).Path
-    $baseDir = (git rev-parse --show-toplevel 2> $null || $currentDir).Replace('/', $IsWindows ? '\' : '/')
-    $footer = $baseDir -eq $currentDir ? $baseDir : "Search:  $baseDir`nCurrent: $currentDir"
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        [string]$BaseDir,
+        [Parameter(ValueFromRemainingArguments)]
+        [string[]]$FdArgs
+    )
 
-    Set-Location $baseDir
+    $currentDir = (Get-Location -PSProvider FileSystem).ProviderPath
+    $BaseDir = $BaseDir -eq '' ? (git rev-parse --show-toplevel 2> $null || $currentDir).Replace('/', $IsWindows ? '\' : '/') : $BaseDir
+    $footer = $BaseDir -eq $currentDir ? $BaseDir : "Scope:   $BaseDir`nCurrent: $currentDir"
 
-    $selectedDir = (& { '.'; fd --type=d --color=always @args } @args
+    Set-Location $BaseDir
+
+    $selectedDir = (& { '.'; fd --type=d --color=always @FdArgs } @FdArgs
         | fzf --ansi --scheme=path --footer="$footer" --preview='fd --max-depth=1 --unrestricted --relative-path --color=always --base-directory {}'
     )
 
-    Set-Location ($selectedDir ? (Resolve-Path -Path $selectedDir -RelativeBasePath $baseDir) : $currentDir)
+    Set-Location ($selectedDir ? (Resolve-Path -Path $selectedDir -RelativeBasePath $BaseDir) : $currentDir)
+}
+
+# cdd: cdf in current directory
+function cdd {
+    cdf -BaseDir (Get-Location -PSProvider FileSystem).ProviderPath @args
 }
